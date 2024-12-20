b36931270ebb487db5d8aafa5ba3d78f532f7d72
yussak <yusuke.sakuraba0213@gmail.com>
2024-12-20 23:35:30 +0900

    正常系のテスト追加

---
+++ src/Domain/models/Book/BookId/BookId.test.ts
@@ -0,0 +1,18 @@
+import { BookId } from "./BookId";
+
+describe('BookId', () => {
+  // 正常系
+  test('有効なフォーマットの場合正しい変換結果を期待', () => {
+    expect(new BookId('9784167158057').value).toBe('9784167158057');
+    expect(new BookId('4167158051').value).toBe('4167158051');
+  });
+
+  test('equals', () => {
+    const bookId1 = new BookId('9784167158057');
+    const bookId2 = new BookId('9784167158057');
+    const bookId3 = new BookId('9781234567890');
+
+    expect(bookId1.equals(bookId2)).toBeTruthy();
+    expect(bookId1.equals(bookId3)).toBeFalsy();
+  })
+})
