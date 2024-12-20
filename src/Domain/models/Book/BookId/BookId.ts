8bff6c45522db839ca0c2467ffba6051d9902f6d
yussak <yusuke.sakuraba0213@gmail.com>
2024-12-20 23:32:14 +0900

    事前にバリデーションをかけるよう変更

--- src/Domain/models/Book/BookId/BookId.ts
+++ src/Domain/models/Book/BookId/BookId.ts
@@ -4,7 +4,31 @@ export class BookId {
   private readonly _value: string;
 
   constructor(value: string) {
+    this.validate(value);
     this._value = value;
+  }
+
+  static MAX_LENGTH = 13;
+  static MIN_LENGTH = 10;
+
+  private validate(isbn: string): void {
+    if (isbn.length < BookId.MIN_LENGTH || isbn.length > BookId.MAX_LENGTH) {
+      throw new Error("ISBNの文字数が不正です");
+    }
+
+    if (!this.isValidIsbn10(isbn) && !this.isValidIsbn13(isbn)) {
+      throw new Error("不正なISBNの形式です")
+    }
+  }
+
+  private isValidIsbn10(isbn10: string): boolean {
+    // 実際にはちゃんとロジックを書く
+    return isbn10.length === 10;
+  }
+
+  private isValidIsbn13(isbn13: string): boolean {
+    // 実際にはちゃんとロジックを書く
+    return isbn13.length === 13;
   }
 
   equals(other: BookId): boolean {
