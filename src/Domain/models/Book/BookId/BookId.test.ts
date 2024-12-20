be554a87d68d6c0dabe67a7835b7e202647acbb3
yussak <yusuke.sakuraba0213@gmail.com>
2024-12-20 23:38:48 +0900

    toISBN()追加

--- src/Domain/models/Book/BookId/BookId.test.ts
+++ src/Domain/models/Book/BookId/BookId.test.ts
@@ -14,5 +14,15 @@ describe('BookId', () => {
 
     expect(bookId1.equals(bookId2)).toBeTruthy();
     expect(bookId1.equals(bookId3)).toBeFalsy();
-  })
-})
+  });
+
+  test('toISBN() 13桁', () => {
+    const bookId = new BookId('9784167158057');
+    expect(bookId.toISBN()).toBe('ISBN978-4-16-715805-7');
+  });
+
+  test('toISBN() 10桁', () => {
+    const bookId = new BookId('4167158051');
+    expect(bookId.toISBN()).toBe('ISBN4-16-715805-1');
+  });
+});
--- src/Domain/models/Book/BookId/BookId.ts
+++ src/Domain/models/Book/BookId/BookId.ts
@@ -38,6 +38,27 @@ export class BookId {
   get value(): string {
     return this._value;
   }
+
+  toISBN(): string {
+    if (this._value.length === 10) {
+      // ISBNが10桁の場合の、'ISBN' フォーマットに変換します。
+      const groupIdentifier = this._value.substring(0, 1); // 国コードなど（1桁）
+      const publisherCode = this._value.substring(1, 3); // 出版者コード（2桁）
+      const bookCode = this._value.substring(3, 9); // 書籍コード（6桁）
+      const checksum = this._value.substring(9); // チェックディジット（1桁）
+
+      return `ISBN${groupIdentifier}-${publisherCode}-${bookCode}-${checksum}`;
+    } else {
+      // ISBNが13桁の場合の、'ISBN' フォーマットに変換します。
+      const isbnPrefix = this._value.substring(0, 3); // 最初の3桁 (978 または 979)
+      const groupIdentifier = this._value.substring(3, 4); // 国コードなど（1桁）
+      const publisherCode = this._value.substring(4, 6); // 出版者コード（2桁）
+      const bookCode = this._value.substring(6, 12); // 書籍コード（6桁）
+      const checksum = this._value.substring(12); // チェックディジット（1桁）
+
+      return `ISBN${isbnPrefix}-${groupIdentifier}-${publisherCode}-${bookCode}-${checksum}`;
+    }
+  }
 }
 
 // 値オブジェクト同士の比較は===じゃなくequalsを使用する。new BookId('9784167158057').value===new BookId('9784167158057').valueとすると値の値を比較となってしまう。値オブジェクトは値なので
